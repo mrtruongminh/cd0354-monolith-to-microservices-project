99a84ceb2215cee799abb6922a9596db69b2dc29
mrtruongminh <nguyentruongminh0604@gmail.com>
2024-04-25 18:24:11 +0700

    submit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http:// a209dc1de4c444dff954ccb86178f451-1466956022.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
